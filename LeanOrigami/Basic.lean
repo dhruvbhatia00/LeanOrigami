d2c73a2018f4292b2a7132b1ec6d9bfd6b8a7bb2
maryam7737 <139247507+maryam7737i@users.noreply.github.com>
2026-05-09 13:44:15 -0700

    Update Basic.lean

--- LeanOrigami/Basic.lean
+++ LeanOrigami/Basic.lean
@@ -1,13 +1,13 @@
-import Mathlib.AlgebraicTopology.SimplexCategory.Basic
-import Mathlib.Data.Fin.VecNotation
-import Mathlib.Data.Real.Basic
-import Mathlib.Tactic
+import Mathlib
 
-def Point : Type := Fin 2 → ℝ
+-- import Mathlib.AlgebraicTopology.SimplexCategory.Basic
+--  import Mathlib.Analysis.RCLike.Basic
 
-def Line : Type := Fin 3 → ℝ
+abbrev Point : Type := Fin 2 → ℝ
 
-def valid (L : Line) : Prop := L 0 ≠ 0 ∨ L 1 ≠ 0
+abbrev Line : Type := Fin 3 → ℝ
+
+def valid (L : Line) : Prop := (L 0)^2 + (L 1)^2 ≠ 0
 
 def is_contained (L : Line) (P : Point) : Prop :=
   (L 0) * (P 0) + (L 1) * (P 1) = L 2
@@ -30,9 +30,22 @@ def reflect : Point :=
   ![x - (2 * a * (a * x + b * y - c))/(a^2 + b^2),
     y - (2 * b * (a * x + b * y - c))/(a^2 + b^2)]
 
-lemma reflect_fix_of_contains (hLP : is_contained L P) :
+lemma reflect_fix_of_contains (hLP : is_contained L P) (h : (L 0) ^ 2 + (L 1) ^ 2 ≠ 0) :
   reflect L P = P := by
     simp [reflect, is_contained] at *;
+    ext i; by_cases hi : i = 0;
+    · simp [hi, h]; right;
+      rw [hLP]; simp
+    · sorry;
+
+def intersects_at : Prop :=
+  is_contained L₁ P ∧ is_contained L₂ P
+
+lemma intersection_coords :
+  let a₁ := L₁ 0; let b₁ := L₁ 1; let c₁ := L₁ 2
+  let a₂ := L₂ 0; let b₂ := L₂ 1; let c₂ := L₂ 2
+intersects_at L₁ L₂ P → P = ![(b₂*c₁ - b₁*c₂)/(a₁*b₂ - a₂*b₁), (c₂* a₁ - c₁ * a₂)/(a₁ * b₂ - b₁ * a₂)] :=
+sorry
 
 
 
