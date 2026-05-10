e4e3ff833b01d857d81ad5fa9b21b100c8d25432
mallorydolorfino <mallod2@uw.edu>
2026-05-10 10:34:40 -0700

    changes to field axioms

--- LeanOrigami/field_axioms_MM.lean
+++ LeanOrigami/field_axioms_MM.lean
@@ -2,7 +2,11 @@ import LeanOrigami.Basic
 import LeanOrigami.constructible
 import LeanOrigami.Axioms
 
+lemma x_axis_cons : cons_line ![0, 1, 0] := by
+  have cons_point ![0 , 0] := by
+    cons_point_origin
 
-lemma (P : Point) : is_constructible_proj P
+
+lemma negative_cons (P : Point) : is_constructible_proj P
  → is_constructible ![-(P 0), -(P 1)] := by
  sorry
